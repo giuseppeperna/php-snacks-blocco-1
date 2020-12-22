5f55af1aadc6dcd9850c5e02513fd6eb18a53b33
giuseppeperna <giuseppeperna.cg@gmail.com>
2020-12-22 14:51:32 +0100

    Start

---
+++ snack_1/index.php
@@ -0,0 +1,6 @@
+<?php 
+/* Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 */
+
+?>
