0e70365b89ab12db7b4b4fa4a1bbfcf84f495161
giuseppeperna <giuseppeperna.cg@gmail.com>
2020-12-22 15:52:44 +0100

    Snack 1

--- snack_1/index.php
+++ snack_1/index.php
@@ -3,4 +3,34 @@
 Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55-60 */
 
+$matches = [
+    'primo_match' => [
+        "casa" => "Olimpia Milano",
+        "fuori" => "Cantù",
+        "punti_casa" => 55,
+        "punti_trasferta" => 60
+    ],
+    'secondo_match' => [
+        "casa" => "Virtus Bologna",
+        "fuori" => "Vanoli Cremona",
+        "punti_casa" => 70,
+        "punti_trasferta" => 65
+    ],
+    'terzo_match' => [
+        "casa" => "Sassari",
+        "fuori" => "Reyer Venezia",
+        "punti_casa" => 80,
+        "punti_trasferta" => 57
+    ],
+    'quarto_match' => [
+        "casa" => "Treviso",
+        "fuori" => "Varese",
+        "punti_casa" => 62,
+        "punti_trasferta" => 70
+    ]
+];
 ?>
+
+<div><?php foreach($matches as $match) {
+    echo $match['casa'] . " - " . $match['fuori'] . " | " . $match['punti_casa'] . "-" . $match['punti_trasferta'] . "<br>";
+} ?></div>
