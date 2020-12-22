90025c7d8d021fc17ddc20be2d30eb262c5220df
giuseppeperna <giuseppeperna.cg@gmail.com>
2020-12-22 16:02:59 +0100

    Start Snack 2

---
+++ snack_2/index.php
@@ -0,0 +1,8 @@
+<?php 
+/*Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. che mail contenga un punto e una chiocciola
+3. e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/
+
+?>
